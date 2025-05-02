27a4da6c7e71ea5f64c9f949f18b3944d47853ea
Evan Nikitin <48099825+radiopushka@users.noreply.github.com>
2025-05-02 00:59:53 -0700

    vocal clarity improvements

---
+++ DEFAULTS.h
@@ -0,0 +1,38 @@
+#ifndef DEFAULTS
+#define DEFAULTS
+
+//default frequency muxer:
+int fdef[]={190,500,3000,6000,15000};
+int fdef_size=5;
+
+float def_attack[]={0.0000001,0.00002,0.000014,0.00001,0.00005};
+float def_release[]={0.000000000001,0.0000000001,0.00000000000001,0.0000000000000001,0.00004};
+float def_target[]={11000,11000,7000,7000,9000};
+float def_m_gain[]={400,400,400,400,400};
+float def_gate[]={6000,3000,4000,4000,130};
+int bypass[]={0,0,0,0,0};
+float post_amp[]={1,1,1,1,1};
+int types[]={COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS,COMP_RMS};
+
+#define FINAL_AMP 1
+#define FINAL_CLIP
+
+
+#define RECORDING_IFACE "default"
+#define PLAYBACK_IFACE "default"
+#define RATE 48000
+
+// 0 is false 1 is true
+#define STEREO 1
+#define STEREO_GAIN 1.6
+
+#define AGC_TARG 4000
+
+#define AGC_SPEED 0.00007
+#define AGC_GATE 1000
+
+// set to 1 to show the levels in real time, 0 to keep silent
+#define GUI 0
+
+
+#endif // !DEFAULTS
